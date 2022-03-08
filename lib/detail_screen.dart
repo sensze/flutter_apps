3c344acda9af7e9acd2d0180f0854cbbc634697e
sensze <ifkusyobaa@gmail.com>
2022-03-08 14:30:57 +0700

    Praktikum 3 Flutter Layout_revisi

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,3 +1,142 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_apps/detail_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'detail_screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const DetailScreen(),
+    );
+  }
+}
+class DetailScreen extends StatelessWidget{
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/monkasel.jpg',
+              height: 230,
+              fit: BoxFit.fitWidth,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 16.0),
+              child: const Text(
+                "Surabaya Submarine Monument",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 33.0,
+                  fontFamily: 'Lobster',
+                ),
+              ),
+            ), //Container untuk Judul
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: const <Widget> [
+                      Icon(Icons.calendar_today),
+                      Text(
+                        'Open Everyday !',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: const <Widget> [
+                      Icon(Icons.access_time_rounded),
+                      Text(
+                        '08.00 - 16.00',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: const <Widget> [
+                      Icon(Icons.attach_money_rounded),
+                      Text(
+                        'Rp 5.000,-',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                "Museum inside a decommisioned Russian war submarine with tours & adjacent park with cafes. Clean and well maintained. Car park cost 10k, entrance fee 15k/person.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.normal,
+                  fontFamily: 'Oxygen',
+                ),
+              ),
+            ), //Container untuk Deskripsi
+            Container(
+                height: 130,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.network('https://eastjavatrip.id/wp-content/uploads/2021/04/Monkasel-5.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset('assets/images/monkasel_2.webp'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset('assets/images/monkasel_3.jpg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset('assets/images/monkasel_4.jpg'),
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
--- lib/main.dart
+++ lib/main.dart
@@ -7,136 +7,154 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'detail_screen',
+      title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const DetailScreen(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-class DetailScreen extends StatelessWidget{
-  const DetailScreen({Key? key}) : super(key: key);
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  Widget build(BuildContext context){
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _text = 'Ketuk untuk menampilkan hasil ';
+  String text2 = 'Prima: ';
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+      _text = '';
+
+
+      //pengkondisian counter
+      if(_counter > 100){
+        _counter = 1;
+      }
+
+      //Bilangan Ganjil Genap
+      // if(_counter%2 == 0){
+      //   _text = "Genap";
+      // }else{
+      //   _text = "Ganjil";
+      // }
+
+      //Bilangan Kelipatan 3 genap
+      // for (int i = 1; i <= _counter; i++){
+      //   if(i % 3 == 0 && i % 2 == 0) {
+      //     _text += '$i, ';
+      //   }
+      // }
+
+      //Bilangan Prima
+      int pembagi = 0;
+      for(int i = 1; i <= _counter; i++){
+        if(_counter%i == 0){
+          pembagi++;
+        }
+      }
+
+      if(pembagi==2){
+        text2+='${_counter}, ';
+      }
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      body: SafeArea(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset(
-                'assets/images/monkasel.jpg',
-              height: 230,
-              fit: BoxFit.fitWidth,
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            Container(
-              margin: EdgeInsets.only(top: 16.0),
-              child: const Text(
-                "Surabaya Submarine Monument",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 33.0,
-                  fontFamily: 'Lobster',
-                ),
-              ),
-            ), //Container untuk Judul
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: const <Widget> [
-                      Icon(Icons.calendar_today),
-                      Text(
-                        'Open Everyday !',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: const <Widget> [
-                      Icon(Icons.access_time_rounded),
-                      Text(
-                        '08.00 - 16.00',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: const <Widget> [
-                      Icon(Icons.attach_money_rounded),
-                      Text(
-                        'Rp 5.000,-',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
             ),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: const Text(
-                "Museum inside a decommisioned Russian war submarine with tours & adjacent park with cafes. Clean and well maintained. Car park cost 10k, entrance fee 15k/person.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.normal,
-                  fontFamily: 'Oxygen',
-                ),
-              ),
-            ), //Container untuk Deskripsi
-            Container(
-              height: 130,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Image.network('https://eastjavatrip.id/wp-content/uploads/2021/04/Monkasel-5.jpg'),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Image.asset('assets/images/monkasel_2.webp'),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Image.asset('assets/images/monkasel_3.jpg'),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Image.asset('assets/images/monkasel_4.jpg'),
-                    ),
-                  ),
-                ],
-              )
+            Text(
+              _text,
+              style: Theme.of(context).textTheme.headline4,
             ),
+            Text(
+              text2,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Tambahkan',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-
